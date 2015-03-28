9d3ab2930e12c43cbb3e5e678eb65ac5456bf524
briankeane <lonesomewhistle@gmail.com>
2015-03-28 10:55:26 -0500

    CameraController

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Brian D Keane on 3/28/15.
+//  Copyright (c) 2015 Brian D Keane. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
